ab4b17382f6289c6d5be65db3cc0b5e64e5a8d79
Damian <damian@ninetyblack.com>
2019-07-10 11:00:08 +1200

    Modifying HasCustomField Trait to access Value

--- src/Model/Traits/HasCustomFields.php
+++ src/Model/Traits/HasCustomFields.php
@@ -23,10 +23,7 @@ trait HasCustomFields
         $customFields = $this->connector->customField()->{$this->customFieldConnectorMethodName()}($this);
 
         foreach ($customFields as $customField) {
-            if ($customField->Type != "") {
-                $this->customFields[$customField->Name] = $customField->{$customField->Type};
-            }
-
+            $this->customFields[$customField->Name] = $customField->Value;
         }
 
         $this->hasResolvedCustomFields = true;
