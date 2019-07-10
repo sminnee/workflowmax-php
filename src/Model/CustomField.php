ed66dfcabaefc508bf594939dbdbcfd1e6df8afb
Damian <damian@ninetyblack.com>
2019-07-10 12:51:45 +1200

    Adding Type back into CustomField

--- src/Model/CustomField.php
+++ src/Model/CustomField.php
@@ -14,6 +14,18 @@ class CustomField
     function processData($data)
     {
 
+        $data['Type'] = "";
+        if (isset($data['Date']))
+            $data['Type'] = "Date";
+        if (isset($data['Number']))
+            $data['Type'] = "Number";
+        if (isset($data['Decimal']))
+            $data['Type'] = "Decimal";
+        if (isset($data['Boolean']))
+            $data['Type'] = "Boolean";
+        if (isset($data['Text']))
+            $data['Type'] = "Text";
+
         $data['Value'] = $data[$data['Type']];
 
 
